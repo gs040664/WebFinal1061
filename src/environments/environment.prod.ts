7a40010b2b2ebce88298c14dd99dc924c228c4b1
Jason Tsai <jasonexe5@gmail.com>
2017-12-27 19:09:24 +0800

    add firebase setting to env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBiWXd74kXEf-iux0m98c4OocF_b4SCDNA',
+    authDomain: 'web-final-1061.firebaseapp.com',
+    databaseURL: 'https://web-final-1061.firebaseio.com',
+    projectId: 'web-final-1061',
+    storageBucket: 'web-final-1061.appspot.com',
+    messagingSenderId: '353034747594'
+  }
 };
